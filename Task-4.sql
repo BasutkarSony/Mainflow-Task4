59af4886537e72feeb4067af9f0980234ed6eb31
BasutkarSony <basutkarsony14@gmail.com>
2025-02-23 17:57:22 +0530

    Add files via upload

--- Task-4.sql
+++ Task-4.sql
@@ -2,8 +2,10 @@ use studentmanagement;
 select studentID,Name,MathScore,TotalScore from students
 order by TotalScore desc;
 
+# Rank Students Based on Total Scores
 select name, TotalScore,rank() over (order by TotalScore desc)  as Ranks
 from students;
 
+# Calculate Running Totals for Math Scores
 select SUM(MathScore) over (order by StudentID) as TotalMathScore
 from students;
